1231c0ad82ac95ab3d08e1eedcab1d5aad7d550f
Roman <bilkasnikers@gmail.com>
2022-02-10 00:08:17 +0200

    finish project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems1 = document.querySelectorAll('.parallax');
     var instances1 = M.Parallax.init(elems1, {});
+
+
+    var elems2 = document.querySelectorAll('.sidenav');
+    var instances2 = M.Sidenav.init(elems2, {});
+
+
+
+    var elems3 = document.querySelectorAll('.dropdown-trigger');
+    var instances3 = M.Dropdown.init(elems3, {});
 });
